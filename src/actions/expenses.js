da42d75862f5bd98ab0fd0a21f09988f6a0a0d84
Adrian McMillian <adrianmcmillian@Adrians-MacBook-Pro.local>
2020-11-30 19:42:12 -0800

    Setup test db env

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,23 +1,45 @@
+import { database } from 'firebase';
 import uuid from 'uuid';
+import db from '../firebase/firebase';
+
+/*
+Without DB...
+Component calls Action Generator
+Action Generator returns Object
+Component dispatches Object
+Redux Store changes
+
+With DB...
+Component calls Action Generator
+Action Generator returns Function
+Component dispatches Function
+Function runs (has the ability to dispatch other Actions)
+*/
+
 
 // ADD_EXPENSE
-export const addExpense = (
-    {
-        description = '',
-        note = '',
-        amount = 0,
-        createdOn = 0
-    } = {}
-) => ({
-type: 'ADD_EXPENSE',
-expense: {
-    id: uuid(),
-    description,
-    note,
-    amount,
-    createdOn
-}
-})
+export const addExpense = (expense) => ({
+    type: 'ADD_EXPENSE',
+    expense
+});
+
+export const startAddExpense = (expenseData = {}) => {
+    return (dispatch) => {
+        const {
+            description = '',
+            note = '',
+            amount = 0,
+            createdOn = 0
+        } = expenseData;
+        const expense = {description, note, amount, createdOn};
+        return db.ref('expenses').push(expense).then((ref) => {
+            dispatch(addExpense({
+                id: ref.key,
+                ...expense
+            }));
+        });
+    };
+};
 
 // REMOVE_EXPENSE
 export const removeExpense = ({id}) => ({
---
+++ src/firebase/firebase.js
@@ -0,0 +1,148 @@
+import * as firebase from 'firebase';
+
+  const config = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  };
+
+  firebase.initializeApp(config);
+
+  const db = firebase.database();
+
+  export {firebase, db as default};
+
+  // db.ref().remove();
+
+  //   db.ref('expenses').push({
+  //   description: 'utility',
+  //   note: 'electricity',
+  //   amount: 2344,
+  //   createdOn: 234234
+  // });
+
+  // db.ref('expenses').push({
+  //   description: 'entertainment',
+  //   note: 'dancing',
+  //   amount: 500,
+  //   createdOn: 234234
+  // });
+
+  // db.ref('expenses').on('child_removed', (snapshot) => {
+  //   console.log(snapshot.key, snapshot.val());
+  // });
+
+  // db.ref('expenses').on('child_changed', (snapshot) => {
+  //   console.log(snapshot.key, snapshot.val());
+  // });
+
+  // db.ref('expenses').on('child_added', (snapshot) => {
+  //   console.log(snapshot.key, snapshot.val());
+  // });
+
+  // db.ref('expenses')
+  //   .once('value')
+  //   .then((snapshot) => {
+  //     const expenses = [];
+  //     snapshot.forEach((childSnapshot) => {
+  //       expenses.push({
+  //         id: childSnapshot.key,
+  //         ...childSnapshot.val()
+  //       });
+  //     });
+  //     console.log(expenses);
+  //   });
+
+  // db.ref('expenses')
+  //   .on('value', (snapshot) => {
+  //     const expenseList = [];
+  //     snapshot.forEach((childSnapshot) => {
+  //       expenseList.push({
+  //         id: childSnapshot.key,
+  //         ...childSnapshot.val()
+  //       });
+  //     });
+  //     console.log(expenseList);
+  //   });
+
+  // db.ref('notes').push({
+  //      title: 'Second note!',
+  //       body: 'More notes...'
+  // })
+
+  // db.ref().on('value', (snapshot) => {
+  //   const val = snapshot.val();
+  //   const name = val.name;
+  //   const job = val.job.title;
+  //   const company = val.job.company;
+  //   console.log(name + ' is a ' + job + ' at ' + company);
+  // });
+
+  // setTimeout(() => {
+  //   db.ref().update({
+  //     'job/title': 'CEO'
+  //   });
+  // },3500);
+
+  // const onValChange = db.ref().on('value', (snapshot) => {
+  //   console.log(snapshot.val());
+  // }, (e) => {
+  //   console.log('Error w data fetching', e);
+  // });
+
+  // setTimeout(() => {
+  //   db.ref('age').set(28);
+  // }, 3500);
+
+  // setTimeout(() => {
+  //   db.ref().off(onValChange);
+  // }, 7000);
+
+  // setTimeout(() => {
+  //   db.ref('age').set(29);
+  // }, 10500);
+
+  // db.ref('location/city')
+  //   .once('value')
+  //   .then((snapshot) => {
+  //       const val = snapshot.val();
+  //       console.log(val);
+  //   })
+  //   .catch((e) => {
+  //       console.log('Error fetching data', e);
+  //   })
+
+//   db.ref().set({
+//       name: 'Adrian McMillian',
+//       age: 51,
+//       stressLevel: 6,
+//       job: {
+//           title: 'Software Dev',
+//           company: 'Google'
+//       },
+//       location: {
+//           city: 'Marina del Rey',
+//           country: 'US'
+//       }
+//   }).then(() => {
+//     console.log('data saved!');
+//   }).catch((e) => {
+//     console.log('error',e);
+//   });
+
+//   db.ref().update({
+//       stressLevel: 9,
+//       'job/company': 'Amazon',
+//       'location/city': 'Seattle'
+//   })
+
+// db.ref()
+// .remove()
+// .then(() => {
+//     console.log('data removed');
+// }).catch((e) => {
+//     console.log('error', e);
+// })
